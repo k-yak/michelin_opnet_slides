666c7a1389f2dbe97f0d1526f4c585b3f868f99f
Moz <florian.rotagnon@gmail.com>
2014-03-14 19:04:47 +0100

    add google io template 2013

---
+++ slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Alternance APM - Michelin',
+    subtitle: 'Septembre 2013 - Mars 2014',
+    //eventTitle: 'Google I/O 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Florian Rotagnon ZZ3 F2',
+    company: 'Alternant Infra/group, Michelin',
+    www: 'http://fr.linkedin.com/in/florianrotagnon/',
+	gplus: 'florian.rotagnon@gmail.com',
+	twitter: '@F10_R',
+	github: 'http://github.com/moz'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
